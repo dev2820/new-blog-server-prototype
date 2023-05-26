8e260fa4cd9486eb6c0779c9f65efd1f9eb5ac03
dev2820 <second28200@gmail.com>
2023-05-26 20:21:57 +0900

    chore/ temp server

---
+++ router/index.js
@@ -0,0 +1,21 @@
+const router = require("@koa/router")();
+const passport = require("koa-passport");
+
+router.get("/", async (ctx) => {
+  ctx.body = { message: "hello world" };
+});
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (ctx) => {
+    ctx.redirect("https://new-blog-web-prototype.vercel.app/");
+  }
+);
+
+module.exports = router;
