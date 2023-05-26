e1e8238962859c29f900c3bb9bb56e452e772959
dev2820 <second28200@gmail.com>
2023-05-26 21:33:42 +0900

    refactor/ comment unused codes

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const Koa = require("koa");
-const passport = require("koa-passport");
-const GoogleStrategy = require("passport-google-oauth20");
+// const passport = require("koa-passport");
+// const GoogleStrategy = require("passport-google-oauth20");
 require("dotenv").config();
 
 const app = new Koa();
 const router = require("./router");
 const PORT = process.env.PORT;
 
-app.use(passport.initialize());
+// app.use(passport.initialize());
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile);
-      return done(null, profile);
-    }
-  )
-);
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: process.env.GOOGLE_CALLBACK_URL,
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       console.log(accessToken, refreshToken, profile);
+//       return done(null, profile);
+//     }
+//   )
+// );
 
 app.use(router.routes());
 app.use(router.allowedMethods());
