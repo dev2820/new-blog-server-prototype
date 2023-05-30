b1ac5e945755c0745ce69f2681aaa2b39053eab7
dev2820 <second28200@gmail.com>
2023-05-30 16:08:32 +0900

    refactor/ reorder code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Koa = require("koa");
 const cors = require("@koa/cors");
 const { passport } = require("./middlewares");
+const router = require("./router");
 require("dotenv").config();
 
 const app = new Koa();
-const router = require("./router");
 const PORT = process.env.PORT;
 
 app.use(cors());
