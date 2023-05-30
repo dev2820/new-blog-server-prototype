97f85e055ea9ffd0e511322eff51e33b2587ff73
dev2820 <second28200@gmail.com>
2023-05-30 16:18:22 +0900

    refactor/ separate strategy folder

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -1,6 +1,5 @@
 const passport = require("koa-passport");
-const GoogleStrategy = require("passport-google-oauth20");
-
+const googleStrategy = require("./strategies/google");
 /**
  * add passport middleware to app
  * @param app koa application
@@ -9,31 +8,7 @@ const GoogleStrategy = require("passport-google-oauth20");
 const registPassport = (app) => {
   app.use(passport.initialize());
 
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
-      },
-      (accessToken, refreshToken, profile, done) => {
-        /**
-         * TODO:
-         * provider와 id를 조합해 유저를 식별한다.
-         *
-         * 계정을 조회하고 있다면 기존 유저, 없다면 신규 유저이다.
-         * 신규 유저라면 User DB에 저장한다.
-         * refreshToken이 발급되었다면 Auth DB에 저장한다. (redis)
-         * accessToken과 profile을 반환한다.
-         * 이제 client는 bearer 헤더에 accessToken을 저장해 같이 보내야한다.
-         */
-        console.log("accessToken:", accessToken);
-        console.log("refreshToken:", refreshToken);
-        console.log("profile:", profile);
-        return done(null, profile);
-      }
-    )
-  );
+  passport.use(googleStrategy);
 
   return app;
 };
@@ -47,6 +22,7 @@ const registPassport = (app) => {
 const authenticate = (provider, options) => {
   return passport.authenticate(provider, options);
 };
+
 module.exports = {
   registPassport,
   authenticate,
