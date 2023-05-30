06bf5b750ae1e611e8e9385f38eed24e29e756a4
dev2820 <second28200@gmail.com>
2023-05-30 17:53:10 +0900

    fix/ wrong reference about email

--- strategies/google.js
+++ strategies/google.js
@@ -26,7 +26,7 @@ const strategy = new GoogleStrategy(
       User.create(
         profile.displayName,
         profile.id,
-        profile.emails[0],
+        profile.emails[0].value,
         profile.provider
       );
     }
