c6fc9410e73955dbe957d6028fdc52c75339509f
dev2820 <second28200@gmail.com>
2023-05-30 16:39:05 +0900

    refactor/ change googleAuth to GoogleAuth

--- router/api/auth/index.js
+++ router/api/auth/index.js
@@ -1,11 +1,11 @@
 const Router = require("@koa/router");
-const googleAuth = require("./google-auth");
+const GoogleAuth = require("./google-auth");
 const Auth = new Router();
 
 Auth.get("/", async (ctx) => {
   ctx.body = { message: "hello world" };
 });
 
-Auth.use("/google", googleAuth.routes());
+Auth.use("/google", GoogleAuth.routes());
 
 module.exports = Auth;
