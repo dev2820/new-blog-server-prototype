a21522a456fa5dbdeff800e98b2722ff89a3dfce
dev2820 <second28200@gmail.com>
2023-05-30 15:32:52 +0900

    refactor/ separate google-auth login to google.js

---
+++ router/api/auth/google-auth.js
@@ -0,0 +1,29 @@
+const Router = require("@koa/router");
+const passport = require("koa-passport");
+
+const Google = new Router();
+
+Google.get(
+  "/",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    accessType: "offline",
+    prompt: "consent",
+  })
+);
+
+Google.get(
+  "/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/google/callback/failure",
+  }),
+  (ctx) => {
+    ctx.redirect("https://new-blog.store/");
+  }
+);
+
+Google.get("/callback/failure", (ctx) => {
+  ctx.body = "로그인에 실패했습니다!";
+});
+
+module.exports = Google;
--- router/api/auth/index.js
+++ router/api/auth/index.js
@@ -1,33 +1,12 @@
 const Router = require("@koa/router");
 const passport = require("koa-passport");
-
+const googleAuth = require("./google-auth");
 const Auth = new Router();
 
 Auth.get("/", async (ctx) => {
   ctx.body = { message: "hello world" };
 });
 
-Auth.get(
-  "/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-    accessType: "offline",
-    prompt: "consent",
-  })
-);
-
-Auth.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/google/callback/failure",
-  }),
-  (ctx) => {
-    ctx.redirect("https://new-blog.store/");
-  }
-);
-
-Auth.get("/google/callback/failure", (ctx) => {
-  ctx.body = "로그인에 실패했습니다!";
-});
+Auth.use("/google", googleAuth.routes());
 
 module.exports = Auth;
