1d748311e6b822762b6f709d54090120e787d5be
dev2820 <second28200@gmail.com>
2023-06-01 16:31:06 +0900

    Refactor/ remove debug code

--- strategies/local.js
+++ strategies/local.js
@@ -12,7 +12,6 @@ const strategy = new JwtStrategy(options, async (jwtPayload, done) => {
   /**
    * check expired
    */
-  console.log("local strategy works", jwtPayload);
   if (Date.now() >= jwtPayload.exp * 1000) {
     return done(null, false, {
       status: 401,
