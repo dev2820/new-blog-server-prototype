a582bdb5773fe6a35316beead4086ea4b4b2bb7d
dev2820 <second28200@gmail.com>
2023-06-04 22:16:51 +0900

    fix/ add passport reference

--- router/api/user/document/index.js
+++ router/api/user/document/index.js
@@ -1,5 +1,6 @@
 const Router = require("@koa/router");
 const { Client } = require("@notionhq/client");
+const { passport } = require("../../../../middlewares");
 const { Notion } = require("../../../../models");
 const Document = new Router();
 
