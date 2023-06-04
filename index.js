aacae5fb82e0a0a9759fb4dd5ee0a3245d995384
dev2820 <second28200@gmail.com>
2023-06-04 21:53:33 +0900

    feat/ add body parser for params

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const Koa = require("koa");
 const cors = require("@koa/cors");
+const bodyParser = require("koa-bodyparser");
 const { passport } = require("./middlewares");
 const {
   google: googleStrategy,
@@ -12,6 +13,7 @@ const app = new Koa();
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(bodyParser());
 passport.init(app);
 passport.regist(app, [
   { name: googleStrategy.name, strategy: googleStrategy },
