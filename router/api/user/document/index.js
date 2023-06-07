dc746b3f7ce11ee8d2be152db8b58c46c5e71aed
dev2820 <second28200@gmail.com>
2023-06-07 18:09:09 +0900

    feat/ add publish route

--- router/api/user/document/index.js
+++ router/api/user/document/index.js
@@ -6,5 +6,13 @@ const notionRouter = require("./notion");
 const Document = new Router();
 
 Document.use("/notion", notionRouter.routes());
-
+Document.post(
+  "/publish",
+  passport.authenticate("local", { session: false }),
+  async (ctx) => {
+    const { meta, blocks } = ctx.request.body;
+    console.log(meta, blocks);
+    ctx.body = "";
+  }
+);
 module.exports = Document;
