64efe3e13aed13f00ca64106e4d8d180d2435cbf
dev2820 <second28200@gmail.com>
2023-06-07 19:31:25 +0900

    fix/ connect

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const uuid = require("node-uuid");
 
-const client = await mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   dbName: "post",
   useNewUrlParser: true,
   useUnifiedTopology: true,
