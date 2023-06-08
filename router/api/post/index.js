c4991b35ae81416c316648eef5be1620f1c71201
dev2820 <second28200@gmail.com>
2023-06-08 18:52:57 +0900

    refactor/ add debug code

--- router/api/post/index.js
+++ router/api/post/index.js
@@ -4,9 +4,8 @@ const postRouter = new Router();
 
 postRouter.get("/@:author/:title", async (ctx) => {
   const { author, title } = ctx.params;
-  console.log(author, title);
   const post = await Post.findOne({ author, title });
-
+  console.log(post);
   ctx.body = post;
 });
 
