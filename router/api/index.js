9da80a49081b4efb10774ba5d25a35b196de126c
dev2820 <second28200@gmail.com>
2023-06-08 18:20:18 +0900

    feat/ post route

--- router/api/index.js
+++ router/api/index.js
@@ -4,9 +4,11 @@ const api = new Router();
 const auth = require("./auth");
 const user = require("./user");
 const link = require("./link");
+const post = require("./post");
 
 api.use("/auth", auth.routes());
 api.use("/user", user.routes());
 api.use("/link", link.routes());
+api.use("/post", post.routes());
 
 module.exports = api;
--- router/api/post/index.js
+++ router/api/post/index.js
@@ -1,12 +1,12 @@
 const Router = require("@koa/router");
 const { Post } = require("../../../models");
-const documentRouter = new Router();
+const postRouter = new Router();
 
-documentRouter.get("/@:author/:title", async (ctx) => {
+postRouter.get("@:author/:title", async (ctx) => {
   const { author, title } = ctx.params.title;
   const post = await Post.findOne({ author, title });
 
   ctx.body = post;
 });
 
-module.exports = documentRouter;
+module.exports = postRouter;
