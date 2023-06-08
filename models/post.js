84bbfc58f609d199f742246cd5f8276b4ab18792
dev2820 <second28200@gmail.com>
2023-06-08 19:26:28 +0900

    fix/ connection work

--- models/post.js
+++ models/post.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 const uuid = require("node-uuid");
 
-if (!mongoose.connection.readyState) {
-  mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-}
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
