64ac728e0ce131cc053c029760f8f274a2bda818
dev2820 <second28200@gmail.com>
2023-06-08 20:36:13 +0900

    feat/ normalize blocks

--- router/api/post/index.js
+++ router/api/post/index.js
@@ -1,5 +1,6 @@
 const Router = require("@koa/router");
 const { Post } = require("../../../models");
+const normalizer = require("../../../utils/normalizer");
 const postRouter = new Router();
 
 postRouter.get("/@:author/:title", async (ctx) => {
@@ -12,9 +13,27 @@ postRouter.get("/@:author/:title", async (ctx) => {
       author: post.author,
       title: post.title,
     },
-    contents: post.blocks,
+    contents: normalizePageContent(post.blocks),
   };
-  ctx.body = post;
+
+  ctx.body = response;
 });
 
 module.exports = postRouter;
+
+const normalizePageContent = async (rawPageContent) => {
+  const pageContent = rawPageContent.map((block) => {
+    if (block.type === "paragraph") return normalizer.normalizeParagraph(block);
+    if (block.type === "image") return normalizer.normalizeImage(block);
+    if (block.type === "bookmark") return normalizer.normalizeBookmark(block);
+    if (block.type === "heading_1") return normalizer.normalizeHeading1(block);
+    if (block.type === "heading_2") return normalizer.normalizeHeading2(block);
+    if (block.type === "heading_3") return normalizer.normalizeHeading3(block);
+    if (block.type === "bulleted_list_item")
+      return normalizer.normalizeBulletedListItem(block);
+    if (block.type === "quote") return normalizer.normalizeQuote(block);
+    return block;
+  });
+
+  return pageContent;
+};
