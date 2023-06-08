22941798f4ecb59225b390425400f2d9aeef25fe
dev2820 <second28200@gmail.com>
2023-06-08 18:52:05 +0900

    fix/ wrong route

--- router/api/post/index.js
+++ router/api/post/index.js
@@ -2,8 +2,9 @@ const Router = require("@koa/router");
 const { Post } = require("../../../models");
 const postRouter = new Router();
 
-postRouter.get("@:author/:title", async (ctx) => {
-  const { author, title } = ctx.params.title;
+postRouter.get("/@:author/:title", async (ctx) => {
+  const { author, title } = ctx.params;
+  console.log(author, title);
   const post = await Post.findOne({ author, title });
 
   ctx.body = post;
