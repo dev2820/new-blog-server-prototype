57f6ad104a44ea82e564c4a3a9fa339ebb3e8602
dev2820 <second28200@gmail.com>
2023-06-09 20:43:41 +0900

    refactor/ add debug code

--- models/image.js
+++ models/image.js
@@ -8,6 +8,7 @@ const s3 = new AWS.S3({
 });
 
 async function uploadImageFromURL(bucketName, objectKey, imageUrl) {
+  console.log(process.env);
   try {
     const { data } = await axios.get(imageUrl, { responseType: "arraybuffer" });
 
