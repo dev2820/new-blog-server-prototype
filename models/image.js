11b8ac1f75895094bef2024e689fe2b37157bad9
dev2820 <second28200@gmail.com>
2023-06-09 20:48:06 +0900

    fix/ wrong key name

--- models/image.js
+++ models/image.js
@@ -3,7 +3,7 @@ const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_S3_ACCESS_SECRET_KEY,
   region: process.env.AWS_S3_REGION,
 });
 
